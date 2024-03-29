7cc00953ce0e50b7b7bc06e75459a1d84f59e06c
zzziCode <1980136696@qq.com>
2024-03-29 22:36:32 +0800

    项目增加网关 :skull:

---
+++ gateway/src/main/java/com/zzzi/gateway/GatewayApplication.java
@@ -0,0 +1,15 @@
+package com.zzzi.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
